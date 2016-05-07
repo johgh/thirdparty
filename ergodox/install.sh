28d04ecd65bd635224d8dd475d18fb687bed19a7
jorge <jhervas@direla.com>
2016-05-07 11:42:52 +0200

    moved layouts to another repository

--- ergodox/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# clone below repositories if necessary
-# cd
-# git clone https://github.com/jackhumbert/qmk_firmware.git
-# git clone https://github.com/johgh/thirdparty.git
-cd ~/thirdparty/ergodox/
-./teensy_setup.sh
--- ergodox/make_layout.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-make clean
-MAPNAME=dvorak_spanish
-make KEYMAP=$MAPNAME
-mv ergodox_ez.hex "$MAPNAME.hex"
